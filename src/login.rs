200a67741f543e2e22c1360498a8e847aed5b146
Lukáš Hozda <luk.hozda@gmail.com>
2017-06-04 11:22:07 +0200

    implement login

--- src/login.rs
+++ src/login.rs
@@ -1,7 +1,59 @@
+use data::UserDB;
 use packets::Packet;
+
+use std::process::exit;
 use std::net::ToSocketAddrs;
 
 pub fn login<A: ToSocketAddrs>(packet: Packet, addr: A)
 {
-	unimplemented!();
+	let name = match packet.name
+	{
+		Some(n) => n,
+		None =>
+		{
+			println!("  error: we don't login nameless people");
+			Packet::error("missing username")
+				.send(addr);
+			return;
+		}
+	};
+
+	let hash = match packet.data
+	{
+		Some(h) => h,
+		None =>
+		{
+			println!("  error: we don't login people with no password");
+			Packet::error("missing password")
+				.send(addr);
+			return;
+		}
+	};
+
+	let user_db = match UserDB::read()
+	{
+		Ok(d) => d,
+		Err(_) =>
+		{
+			println!("  error: failed to read user db");
+			Packet::error("database error")
+				.send(addr);
+			exit(-1);
+		}
+	};
+
+	if user_db.users
+		.unwrap()
+		.iter()
+		.find(|x| x.name == name && x.hash == hash)
+		.is_some()
+	{
+		Packet::register("hello", "there")
+			.send(addr);
+	}
+	else
+	{
+		Packet::error("couldn't login - invalid username or password")
+			.send(addr);
+	}
 }
